89ddadc74cf3fd51a72490bff0410aded816e2cd
Mahesh Tekumani <tekumani.mahesh2@academy.com>
2024-11-12 10:46:52 +0530

    initial commit

---
+++ src/test/java/com/mylogo/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,10 @@
+package com.mylogo.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ApiGatewayApplicationTests {
+
+
+}
